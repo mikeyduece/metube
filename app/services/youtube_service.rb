bd8a3364cf73be88518e4b31a7afaca9929a224e
Mike Heft <mikeheft@gmail.com>
2017-11-01 19:22:18 -0600

    Make Api call to youtube

--- app/services/youtube_service.rb
+++ app/services/youtube_service.rb
@@ -2,16 +2,17 @@ class YoutubeService
 
   def initialize(vid)
     @vid = vid
-    @conn = Faraday.new(url: "https://www.googleapis.com/youtube/v3/") do |faraday|
+    @conn = Faraday.new(url: "https://www.googleapis.com/youtube/v3/search") do |faraday|
       faraday.params["part"] = 'snippet'
-      faraday.params["maxResults"] = '50'
       faraday.params["key"] = "#{ENV['YT_API']}"
+      faraday.params["maxResults"] = '50'
+      faraday.adapter Faraday.default_adapter
     end
   end
 
   def get_url(url)
-    require 'pry'; binding.pry
     response = @conn.get(url)
+    require 'pry'; binding.pry
     JSON.parse(response.body, symbolize_names: true)
   end
 
@@ -20,7 +21,7 @@ class YoutubeService
   end
 
   def search_videos
-    get_url("search?q=#{vid}")
+    get_url("?q=#{vid}")
   end
 
   private
