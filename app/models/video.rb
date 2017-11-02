70e9531392f97eef707a598a99f8281ba6d59ece
Mike Heft <mikeheft@gmail.com>
2017-11-02 07:24:49 -0600

    Add model validation tests

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,6 +2,8 @@ class Video < ApplicationRecord
   has_many :playlists
   has_many :users, through: :playlists
 
+  validates :videoId, uniqueness: true, presence: true
+
   def self.search_videos(vid)
     VideoService.search_videos(vid)
   end
