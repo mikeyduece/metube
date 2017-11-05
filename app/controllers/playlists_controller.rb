58de2d5e05872b7c4d91efa0e7144f6c99bd86c3
Mike Heft <mikeheft@gmail.com>
2017-11-05 11:49:08 -0700

    Work on adding to existing playlist name

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -5,6 +5,7 @@ class PlaylistsController < ApplicationController
   end
 
   def create
+    require 'pry'; binding.pry
     video = VideoService.add_video(params)
     playlist_video = Playlist.add_to_playlist(video, params[:user_id], params[:name])
     current_user.playlists << playlist_video
