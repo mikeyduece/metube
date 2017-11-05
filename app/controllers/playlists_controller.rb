4fe55f8fb108d90201b2f61cfc8d6f48e7de9294
Mike Heft <mikeheft@gmail.com>
2017-11-05 12:09:30 -0700

    Add to playlist with existing name from video card

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -5,6 +5,10 @@ class PlaylistsController < ApplicationController
   end
 
   def create
+    if params['playlist']['name']
+      require 'pry'; binding.pry
+      params[:name] = params['playlist']['name']
+    end
     require 'pry'; binding.pry
     video = VideoService.add_video(params)
     playlist_video = Playlist.add_to_playlist(video, params[:user_id], params[:name])
