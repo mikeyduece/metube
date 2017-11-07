49fd9d7d17de1a8448c4c9c3960c51f242798817
Mike Heft <mikeheft@gmail.com>
2017-11-07 06:20:45 -0700

    Work on iframe

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,8 +19,8 @@
 // import { apiKey } from './helper.js'
 // $(function($) {
 //   $(document).on('click', '#favorite', function(event) {
-//     $(this).find('i').css('color','red').toggleClass('glyphicon-heart-empty').toggleClass('glyphicon-heart');
-//   });
+//     $(this).find('i').css('color','red').toggleClass('glyphicon-heart-empty');
+//     //   });
 // });
 bootcards.init({
   offCanvasBackdrop: true,
@@ -29,42 +29,40 @@ bootcards.init({
   disableRubberBanding: true
 });
 
-(function($) {
-
-    $.fn.bmdIframe = function( options ) {
-        var self = this;
-        var settings = $.extend({
-            classBtn: '.bmd-modalButton',
-            defaultW: 640,
-            defaultH: 360
-        }, options );
-
-        $(settings.classBtn).on('click', function(e) {
-          var allowFullscreen = $(this).attr('data-bmdVideoFullscreen') || false;
-
-          var dataVideo = {
-            'src': $(this).attr('data-bmdSrc'),
-            'height': $(this).attr('data-bmdHeight') || settings.defaultH,
-            'width': $(this).attr('data-bmdWidth') || settings.defaultW
-          };
-
-          if ( allowFullscreen ) dataVideo.allowfullscreen = "";
-
-          // stampiamo i nostri dati nell'iframe
-          $(self).find("iframe").attr(dataVideo);
-        });
-
-        // se si chiude la modale resettiamo i dati dell'iframe per impedire ad un video di continuare a riprodursi anche quando la modale è chiusa
-        this.on('hidden.bs.modal', function(){
-          $(this).find('iframe').html("").attr("src", "");
-        });
-
-        return this;
-    };
-
-})($);
-
-
+// (function($) {
+//
+//     $.fn.bmdIframe = function( options ) {
+//         var self = this;
+//         var settings = $.extend({
+//             classBtn: '.bmd-modalButton',
+//             defaultW: 640,
+//             defaultH: 360
+//         }, options );
+//
+//         $(settings.classBtn).on('click', function(e) {
+//           var allowFullscreen = $(this).attr('data-bmdVideoFullscreen') || false;
+//
+//           var dataVideo = {
+//             'src': $(this).attr('data-bmdSrc'),
+//             'height': $(this).attr('data-bmdHeight') || settings.defaultH,
+//             'width': $(this).attr('data-bmdWidth') || settings.defaultW
+//           };
+//
+//           if ( allowFullscreen ) dataVideo.allowfullscreen = "";
+//
+//           // stampiamo i nostri dati nell'iframe
+//           $(self).find("iframe").attr(dataVideo);
+//         });
+//
+//         // se si chiude la modale resettiamo i dati dell'iframe per impedire ad un video di continuare a riprodursi anche quando la modale è chiusa
+//         this.on('hidden.bs.modal', function(e){
+//           $(this).find('iframe').html("").attr("src", "");
+//         });
+//
+//         return this;
+//     };
+//
+// })($);
 
 
 $(document).ready(function(){
@@ -88,8 +86,18 @@ function onYouTubeIFrameReady() {
 }
 
 function onPlayerReady(event) {
-      event.target.playVideo();
-    };
+  event.target.playVideo();
+  //bind events
+  var playbutton = document.getElementById('play-button');
+  playButton.addEventListent('click', function() {
+    play.playVideo();
+  });
+
+  var pauseButton = document.getElementById('pause-button');
+  pauseBtton.addEventListener('click', function() {
+    player.pauseVideo();
+  });
+};
 
 function onPlayerStateChange(event)
 
